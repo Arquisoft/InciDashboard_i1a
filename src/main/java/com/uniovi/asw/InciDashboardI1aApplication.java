fe2f453adb06ec073395492b6190955f2315ed97
Jorge <jorgegnb197@gmail.com>
2018-03-07 14:08:14 +0100

    Proyecto añadido

---
+++ src/main/java/com/uniovi/asw/InciDashboardI1aApplication.java
@@ -0,0 +1,12 @@
+package com.uniovi.asw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InciDashboardI1aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InciDashboardI1aApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uniovi/asw/InciDashboardI1aApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uniovi.asw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InciDashboardI1aApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
