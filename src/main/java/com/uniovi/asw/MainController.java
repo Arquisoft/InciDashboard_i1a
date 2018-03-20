1a6f87bedae318ab57a5ce63c002698efd3cee42
dromir <uo250757@uniovi.es>
2018-03-20 11:39:57 +0100

    First version of the dashboard, test needed but Kafka doesnt love me

--- src/main/java/com/uniovi/asw/MainController.java
+++ src/main/java/com/uniovi/asw/MainController.java
@@ -1,17 +1,24 @@
 package com.uniovi.asw;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.uniovi.asw.listeners.MessageListener;
+
 @Controller
 public class MainController {
+
+	@Autowired
+	private MessageListener listener;
 
 	private static final Logger logger = Logger.getLogger(MainController.class);
 
 	@RequestMapping("/")
 	public String landing(Model model) {
+		model.addAttribute("messages", listener.getMessages());
 		return "index";
 	}
 
--- src/main/java/com/uniovi/asw/listeners/MessageListener.java
+++ src/main/java/com/uniovi/asw/listeners/MessageListener.java
@@ -1,5 +1,8 @@
 package com.uniovi.asw.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.ManagedBean;
 
 import org.apache.log4j.Logger;
@@ -11,11 +14,17 @@ import org.springframework.kafka.annotation.KafkaListener;
 @ManagedBean
 public class MessageListener {
 
+	List<String> messages = new ArrayList<String>();
 	private static final Logger logger = Logger.getLogger(MessageListener.class);
 
 	@KafkaListener(topics = "exampleTopic")
 	public void listen(String data) {
 		logger.info("New message received: \"" + data + "\"");
+		messages.add(data);
+	}
+
+	public List<String> getMessages() {
+		return messages;
 	}
 
 }
