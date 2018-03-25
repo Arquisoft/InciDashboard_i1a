a33c20e4a7c32f1010ff97448a1743d894f8466b
Roberto Pérez <robertops1818@gmail.com>
2018-03-25 19:12:43 +0200

    Added map with counter of the incidents by topic to the dashboard view through the model
    This will be used to create the bar chart

--- src/main/java/com/uniovi/asw/services/IncidentService.java
+++ src/main/java/com/uniovi/asw/services/IncidentService.java
@@ -1,6 +1,8 @@
 package com.uniovi.asw.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class IncidentService {
 	
 	@Autowired
 	private IncidentRepository incidentRepository;
+	
+	@Autowired
+	private TopicService topicsService;
 
 	public void saveIncident(Incident incident) {
 		incidentRepository.save(incident);
@@ -20,6 +25,29 @@ public class IncidentService {
 
 	public List<Incident> getIncidents() {
 		return incidentRepository.findAll();
+	}
+	
+	public List<Incident> getByTopic(String topic)
+	{
+		return incidentRepository.findByTopic(topic);
+	}
+
+	public Map<String, Integer> getCountByTopic(List<Incident> incidentsList) {
+		Map<String, Integer> count = new HashMap<String, Integer>();
+		String[] topics = topicsService.getTopics();
+		count.put(topics[0], 0);
+		count.put(topics[1], 0);
+		count.put(topics[2], 0);
+		count.put(topics[3], 0);
+		count.put(topics[4], 0);
+		count.put(topics[5], 0);
+		for (Incident i : incidentsList)
+		{
+			int countUntilNow = count.get(i.getTopic());
+			countUntilNow++;
+			count.put(i.getTopic(), countUntilNow);
+		}
+		return count;
 	}
 
 }
