2de3e3f284f23e0f70137223a66c8aa6b641dd0d
Pedro Blanco <peblancos@gmail.com>
2018-03-26 11:33:06 +0200

    Añadida página de detalles sobre un incidente.

--- src/main/java/com/uniovi/asw/controllers/IncidentController.java
+++ src/main/java/com/uniovi/asw/controllers/IncidentController.java
@@ -2,6 +2,9 @@ package com.uniovi.asw.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uniovi.asw.services.IncidentService;
 
@@ -10,5 +13,10 @@ public class IncidentController {
 
 	@Autowired
 	private IncidentService incidentService;
+	
+	@RequestMapping("/incident/details/{id}")
+	public String getDetails(Model model, @PathVariable Long id) {
+		return "incident/details";
+	}
 
 }
