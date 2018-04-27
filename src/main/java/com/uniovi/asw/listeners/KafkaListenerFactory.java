23ffd9628b46871b1f8a5fd1d30b8507d0b0cd1e
Jorge <jorgegnb197@gmail.com>
2018-04-27 18:45:13 +0200

    Fixing project for using Docker

--- src/main/java/com/uniovi/asw/listeners/KafkaListenerFactory.java
+++ src/main/java/com/uniovi/asw/listeners/KafkaListenerFactory.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -20,6 +21,9 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 @Configuration
 @EnableKafka
 public class KafkaListenerFactory {
+	
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServers;
 
 	@Bean
 	KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>> kafkaListenerContainerFactory() {
@@ -38,7 +42,7 @@ public class KafkaListenerFactory {
 	@Bean
 	public Map<String, Object> consumerConfigs() {
 		Map<String, Object> props = new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
 		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
--- src/main/java/com/uniovi/asw/repositories/AgentRepository.java
+++ src/main/java/com/uniovi/asw/repositories/AgentRepository.java
@@ -1,6 +1,7 @@
 package com.uniovi.asw.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,6 @@ import com.uniovi.asw.entities.Agent;
 
 @Repository
 public interface AgentRepository extends MongoRepository<Agent, String> {
-	Agent findById(String id);
+	Optional<Agent> findById(String id);
 	List<Agent> findAll();
 }
