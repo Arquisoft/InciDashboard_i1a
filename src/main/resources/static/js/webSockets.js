63e997146cb7d43bfd1a3a54c717cd576c1ca78e
Roberto Pérez <robertops1818@gmail.com>
2018-05-07 11:53:32 +0200

    Primera aproximacion de web sockets, solamente se actualiza la tabla de incidentes

---
+++ src/main/resources/static/js/webSockets.js
@@ -0,0 +1,39 @@
+function connect(){
+	var socket = new SockJS("/dashboard");
+	stompClient = Stomp.over(socket);
+	stompClient.connect({}, function(frame){		
+		console.log("Connected: " + frame);
+
+		stompClient.subscribe("/incident", function (data) {			
+			var incident = JSON.parse(data.body); 
+	        updateTable(incident);
+		});		
+	});
+}
+
+$(document).ready(function () {
+    connect();
+});
+
+function updateTable(incident){
+	$("#incidentsTable").append(
+		"<tr>"
+		+ "<td>" + incident.idautogenerado + "<td>"
+		+ "<td>" + incident.agent.name + "<td>"
+		+ "<td>" + incident.incidentName + "<td>"
+		+ "<td><a href='http://maps.google.com/maps?q=" + incident.location.lat + "," + incident.location.lng + "'</a><td>"
+		+ "<td>" + incident.date + "<td>"
+		+ "<td>" + incident.topic + "<td>"
+		+ "<td>" + incident.operator.username + "<td>"
+		+ "<td>" + incident.status + "<td>"
+		+ "<td><div class='btn-group' role='group' aria-label='Options'>"
+			+ "<a class='btn btn-secondary' role='button' href='/incident/details/'" + incident.idautogenerado + "'>Details</a>"
+		+ "</div></td>"
+	);
+	
+	if (incident.isAssignedTo(loggedUser)) {
+		$("#options").append(
+				"<a class='btn btn-secondary' role='button' href='/incident/edit/'" + incident.idautogenerado + "'>Change status</a>"
+		);
+	}
+}
